87b48b6dfe6fce99b420b3dad5f893d7401e7c56
yodhanabiha <yodhanabiha2@gmail.com>
2023-11-25 23:31:12 +0700

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,11 @@
-import { Button, StyleSheet, TouchableOpacity } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import React from 'react'
+import { Pressable, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { Image } from 'react-native';
-import { Link, useNavigation } from 'expo-router';
-
-import imageKaos from '../../assets/images/kaos.png';
-import imageKemeja from '../../assets/images/kemeja.png';
-import { StackActions } from '@react-navigation/native';
-
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
-  const navigation = useNavigation()
+  const imageKaos = require("../../assets/images/kaos.png")
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Selamat Datang !</Text>
@@ -22,15 +16,17 @@ export default function TabOneScreen() {
           style={{ width: 150, height: 150 }}
         />
       </View>
-      <View>
-        <TouchableOpacity
-          style={[styles.button]}
-          onPress={() => navigation.navigate("detail")}
-        >
-          <Text style={styles.input}>Beli</Text>
-        </TouchableOpacity>
 
-      </View>
+      <Link href="/detail">
+        <Pressable>
+          <TouchableOpacity
+            style={[styles.button]}
+          >
+            <Text style={styles.input}>Beli</Text>
+          </TouchableOpacity>
+        </Pressable>
+      </Link>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={[styles.row, styles.produk]} />
       <Image
--- app/detail.tsx
+++ app/detail.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function DetailProdukScreen() {
   return (
     <View style={styles.container}>
-      <Text>Add friends here!</Text>
+      <Text>Baju Hits</Text>
     </View>
   );
 
