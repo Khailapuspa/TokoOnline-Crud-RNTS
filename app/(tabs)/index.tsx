506100295a07bf2beb5e7d9bf96717b3ca85f672
yodhanabiha <yodhanabiha2@gmail.com>
2023-11-25 11:19:46 +0700

    add images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,16 @@ import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import { Image } from 'react-native';
 
+import imageKaos from '../../assets/images/kaos.png';
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <Image
-  source={{uri: 'asset:/assets/image/kaos.png'}}
-  style={{width: 40, height: 40}}
-/>
+        source={imageKaos}
+        style={{ width: 40, height: 40 }}
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
